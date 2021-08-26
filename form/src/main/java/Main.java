3e09bd0912431f59c490ff866e839128a0477707
RitanMihai <lucian.ritan@gmail.com>
2021-08-26 15:08:07 +0300

    Started client application
    - Added simple form window without backend
    - Style modified with this css: https://github.com/dicolar/jbootx/blob/master/src/main/resources/bootstrap3.css

---
+++ form/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
